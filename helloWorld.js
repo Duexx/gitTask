fcb1d232c671acbcd49a45c4b6d7530580f0ef33
Jonathan Lee <jono.lee522@gmail.com>
2021-09-30 13:07:31 +0200

    Now receives user input to be displayed back to them

--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,5 @@
-console.log("Git is awesome!");
+//User inputs a message to be displayed back to them
+let userMessage = prompt("Please enter a message to be displayed!");
+
+console.log(userMessage);
+alert(userMessage);
